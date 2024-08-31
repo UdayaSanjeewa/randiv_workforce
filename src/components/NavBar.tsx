c77cc04fcea39e7c5b6b6bf1f2d69ce32abbd433
P A Chathura Madhava <chathuratechdev@gmail.com>
2024-08-31 22:54:58 +0530

    refactor: Add company logo

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,6 +1,6 @@
 import Image from "next/image";
 import React from "react";
-import logo from "@/resources/logo.jpg";
+import logo from "@/resources/rv-logo.jpeg";
 import Link from "next/link";
 
 function NavBar() {
