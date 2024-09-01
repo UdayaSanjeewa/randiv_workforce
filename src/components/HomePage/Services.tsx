7f30a56edcf5fa487d3a20d63aa3d9381ef494fc
SazaO_o <121901716+Saza-dev@users.noreply.github.com>
2024-09-01 15:09:49 +0530

    responsive homepageServices component

--- src/components/HomePage/Services.tsx
+++ src/components/HomePage/Services.tsx
@@ -9,16 +9,16 @@ import Link from "next/link";
 function services() {
   return (
     <div className="flex items-center justify-center">
-      <div className="w-[1300px] h-[500px]">
+      <div className="md:w-[1300px] h-[1500px] md:h-[500px]">
         {/* Heading */}
-        <h2 className="text-[38px] font-[700] text-[#21215F] mb-[20px]">
+        <h2 className="text-[38px] text-center md:text-start mt-[90px] ml-[30px] md:ml-[120px] md:mt-[100px] xl:ml-0 xl:mt-0 font-[700] text-[#21215F] mb-[20px]">
           Services
         </h2>
 
         {/* Cards */}
-        <div className="flex flex-row justify-between">
+        <div className="flex flex-col gap-8 md:gap-0 md:flex-row items-center md:justify-around xl:justify-between p-10 md:p-0">
           {/* Card 01 */}
-          <Link href="/Services" className="relative w-[293px] h-[290px]">
+          <Link href="/Services" className="relative w-[240px] h-[260px] md:w-[190px] md:h-[220px] xl:w-[283px] xl:h-[290px]">
             <Image
               src={serveOne}
               alt="service 01"
@@ -29,15 +29,15 @@ function services() {
             {/* black gradient */}
             <div className="absolute inset-0 bg-gradient-to-t from-black rounded-[10px]"></div>
             {/* topic */}
-            <div className="absolute inset-0 text-center mt-[220px]">
-              <p className="text-white text-[20px] font-[700]">
+            <div className="absolute inset-0 text-center mt-[200px] md:mt-[150px] xl:mt-[220px]">
+              <p className="text-white xl:text-[20px] font-[700]">
                 Work Permit Assistance
               </p>
             </div>
           </Link>
 
           {/* Card 02 */}
-          <Link href="/Services"  className="relative w-[293px] h-[290px]">
+          <Link href="/Services"  className="relative w-[240px] h-[260px] md:w-[190px] md:h-[220px] xl:w-[283px] xl:h-[290px]">
             <Image
               src={serveTwo}
               alt="service 01"
@@ -48,15 +48,15 @@ function services() {
             {/* black gradient */}
             <div className="absolute inset-0 bg-gradient-to-t from-black rounded-[10px]"></div>
             {/* topic */}
-            <div className="absolute inset-0 text-center mt-[220px]">
-              <p className="text-white text-[20px] font-[700] ">
+            <div className="absolute inset-0 text-center mt-[200px] md:mt-[150px] xl:mt-[220px]">
+              <p className="text-white xl:text-[20px] font-[700] ">
                 Financial Assistance for Applicants
               </p>
             </div>
           </Link>
 
           {/* Card 03 */}
-          <Link href="/Services"  className="relative w-[293px] h-[290px]">
+          <Link href="/Services"  className="relative w-[240px] h-[260px] md:w-[190px] md:h-[220px] xl:w-[283px] xl:h-[290px]">
             <Image
               src={serveThree}
               alt="service 01"
@@ -66,8 +66,8 @@ function services() {
             {/* black gradient */}
             <div className="absolute inset-0 bg-gradient-to-t from-black rounded-[10px]"></div>
             {/* topic */}
-            <div className="absolute inset-0 text-center mt-[220px]">
-              <p className="text-white text-[20px] font-[700]">
+            <div className="absolute inset-0 text-center mt-[200px] md:mt-[150px] xl:mt-[220px]">
+              <p className="text-white xl:text-[20px] font-[700]">
                 Career Counseling &
                 <br />
                 Pre-Departure Support
@@ -76,7 +76,7 @@ function services() {
           </Link>
 
           {/* Card 04 */}
-          <Link href="/Services"  className="relative w-[293px] h-[290px]">
+          <Link href="/Services"  className="relative w-[240px] h-[260px] md:w-[190px] md:h-[220px] xl:w-[283px] xl:h-[290px]">
             <Image
               src={serveFour}
               alt="service 01"
@@ -86,8 +86,8 @@ function services() {
             {/* black gradient */}
             <div className="absolute inset-0 bg-gradient-to-t from-black rounded-[10px]"></div>
             {/* topic */}
-            <div className="absolute inset-0 text-center mt-[220px]">
-              <p className="text-white text-[20px] font-[700]">
+            <div className="absolute inset-0 text-center mt-[200px] md:mt-[150px] xl:mt-[220px]">
+              <p className="text-white xl:text-[20px] font-[700]">
                 Job Placement in UK Care Homes
               </p>
             </div>
