be662905d28b5988615c925479485f90decbbf39
SazaO_o <121901716+Saza-dev@users.noreply.github.com>
2024-09-01 15:30:22 +0530

    navbar fixed for lg screens

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -9,14 +9,14 @@ function NavBar() {
 
   return (
     // Main Container
-    <div className="w-full h-[50px] xl:h-[95px] flex justify-around items-center shadow-xl sticky top-0 z-10 bg-white">
+    <div className="w-full h-[50px] lg:h-[95px] flex justify-around items-center shadow-lg sticky top-0 z-10 bg-white">
       {/* Logo */}
-      <div className="w-[87px] xl:w-[125px]">
+      <div className="w-[87px] lg:w-[125px]">
         <Image src={logo} alt="Logo" />
       </div>
 
       {/* Menu Button for Small Screens */}
-      <div className="xl:hidden">
+      <div className="lg:hidden">
         <button
           onClick={() => setIsOpen(!isOpen)}
           className="text-gray-700 focus:outline-none"
@@ -57,29 +57,29 @@ function NavBar() {
       <div
         className={`${
           isOpen ? "block" : "hidden"
-        } xl:flex xl:w-[476px] xl:justify-between xl:text-[20px] xl:font-[700] absolute xl:static top-[100%] left-0 w-full bg-white xl:bg-transparent`}
+        } lg:flex lg:w-[476px] lg:justify-between lg:text-[20px] lg:font-[700] absolute lg:static top-[100%] left-0 w-full bg-white lg:bg-transparent`}
       >
         <Link
           href="/"
-          className="block py-2 px-4 text-center xl:inline-block xl:py-0 xl:px-0"
+          className="block py-2 px-4 text-center lg:inline-block lg:py-0 lg:px-0"
         >
           Home
         </Link>
         <Link
           href="/AboutUs"
-          className="block py-2 px-4 text-center xl:inline-block xl:py-0 xl:px-0"
+          className="block py-2 px-4 text-center lg:inline-block lg:py-0 lg:px-0"
         >
           About Us
         </Link>
         <Link
           href="/Portfolio"
-          className="block py-2 px-4 text-center xl:inline-block xl:py-0 xl:px-0"
+          className="block py-2 px-4 text-center lg:inline-block lg:py-0 lg:px-0"
         >
           Portfolio
         </Link>
         <Link
           href="/ContactUs"
-          className="block py-2 px-4 text-center xl:inline-block xl:py-0 xl:px-0"
+          className="block py-2 px-4 text-center lg:inline-block lg:py-0 lg:px-0"
         >
           Contact Us
         </Link>
