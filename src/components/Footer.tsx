aa084d561ddb4be8ce2fadb48dbb3312672fee34
SazaO_o <121901716+Saza-dev@users.noreply.github.com>
2024-09-01 14:12:26 +0530

    responsive footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -6,19 +6,19 @@ import Link from "next/link";
 function Footer() {
   return (
     // Main container
-    <div className="w-full h-[303px] bg-[#21215F] flex items-center justify-between pr-[400px] bottom-0">
-      {/* logo */}
-      <div className="w-[204px] h-[129px] rounded-r-[80px] bg-white flex items-center justify-center">
-        {/* image */}
+    <div className="w-full bg-[#21215F] h-[650px] flex flex-col md:flex-row md:items-center  py-8  md:py-0 md:h-[303px]">
+      {/* Logo */}
+      <div className="mb-8 md:mb-0 w-[204px] h-[129px] rounded-r-[80px] bg-white flex items-center justify-center">
         <div className="w-[120px]">
-          <Image src={logo} alt=""></Image>
+          <Image src={logo} alt="Logo" />
         </div>
       </div>
-      {/* QuickLinks */}
-      <div className="flex flex-col w-[163px] h-[158px] text-white justify-between">
+
+      {/* Quick Links */}
+      <div className="mb-8 md:mb-0 flex flex-col ml-[30px] items-start text-white gap-2 md:ml-[60px] text-center md:text-left w-[200px] h-[158px] xl:ml-[200px] md:mt-[30px]">
         <h2 className="text-[22px] font-[700]">Quick Links</h2>
         <Link href="/AboutUs" className="opacity-[70%] text-[18px] font-[400]">
-          About us
+          About Us
         </Link>
         <Link
           href="/Portfolio"
@@ -33,29 +33,29 @@ function Footer() {
           Privacy Policy
         </Link>
       </div>
+
       {/* Contact */}
-      <div className="w-[592px] h-[167px] text-white flex flex-col justify-between">
-        <h2 className="text-[22px] font-[700]">Contact</h2>
-        {/* email */}
-        <div className="flex opacity-[70%]">
+      <div className="text-white flex flex-col gap-2 ml-[30px] md:ml-[60px] md:items-start md:text-left w-full md:w-[400px] h-[167px] xl:ml-[200px] md:mt-[34px]">
+        <h2 className="text-[22px] font-[700] mb-4 md:mb-0">Contact</h2>
+        <div className="flex  md:justify-start opacity-[70%] mb-2">
           <p>Email</p>
           <p className="pl-[25px] pr-4">:</p>
-          <p className="opacity-[40%] text-[12px]">info@randivworkforce.com</p>
+          <p className="opacity-[40%] text-[12px]">
+            info@randivworkforce.com
+          </p>
         </div>
-        {/* phone */}
-        <div className="flex opacity-[70%]">
+        <div className="flex  md:justify-start opacity-[70%] mb-2">
           <p>Phone</p>
           <p className="pl-[18px] pr-4">:</p>
           <p className="opacity-[40%] text-[12px]">
             077 374 1720 / 0332 320 490
           </p>
         </div>
-        {/* Address */}
-        <div className="flex opacity-[70%]">
+        <div className="flex md:justify-start opacity-[70%]">
           <p>Address</p>
           <p className="pl-[7px] pr-4">:</p>
-          <p className="opacity-[40%] text-[12px]">
-            Randiv WorkforceForeign Employment Solution(pvt) ltd no 104, 3rd
+          <p className="opacity-[40%] text-[12px] w-[200px] xl:w-[350px]">
+            Randiv Workforce Foreign Employment Solution(pvt) ltd no 104, 3rd
             floor, Sanasa Ward City, Queen Marry Road, Gampaha, Sri Lanka.
           </p>
         </div>
